520f86dda33cd53a592d3e8fbea17ff41f73f198
Wojtek <falcaomatador@gmail.com>
2019-12-08 14:40:40 +0100

    zaktualizowani dostawcy

---
+++ baza.py
@@ -0,0 +1,3 @@
+kurczaki = Dostawca("Kurczaki", "ul. Kurczacza 6, 00-666, Kurczaków", 50)
+ziemniaki = Dostawca("Badyle ziemniaczane", "ul. Ziemniaczana 9, 00-292 Ziemniaków", 20)
+warzywniak = Dostawca("Krwawy burak", "ul. Buraczana 3, 03-333 Buraków", 35)
