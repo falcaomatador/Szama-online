b4c2eb895a966da48047d698478e24375f3a1fe6
unknown <swider1000@wp.pl>
2019-12-08 14:57:07 +0100

    Dostawca 1.3

--- dostawca.py
+++ dostawca.py
@@ -4,7 +4,16 @@ class Dostawca:
         self.adres = adres
         self.min_zam = min_zam
 
+class Menu:
+    def __init__(self, dostawca, produkt, cena):
+        self.produkt = produkt
+        self.cena = cena
+        self. dostawca = dostawca
 
-class Menu(Dostawca):
-    def __init__(self, menu, produkt, cena):
-        super().__init__(menu, produkt, cena).
+class Produkt:
+    def __init__(self, nazwa, cena):
+        self.nazwa = nazwa
+        self.cena = cena
+
+        def __str__(self):
+            return f'{self.nazwa}, {self.cena} zł'
